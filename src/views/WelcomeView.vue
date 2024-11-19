dfa9f58ef80f7c11a27cd1cf46481535e279134f
kiw1meister <linandy719@gmail.com>
2024-11-19 12:28:11 +0900

    feat: clear scaffolding + create welcome page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
+    <div class="w-1/2 text-center">
+      <h1 class="text-5xl">Welcome to Japuri</h1>
+      <h2 class="mt-5">Cultural appreciation begins with understanding</h2>
+    </div>
+    <div class="mx-auto mt-10">
+      <button class="bg-blueGreen p-3 rounded-xl mx-5">Login</button>
+      <button class="bg-blueGreen p-3 rounded-xl mx-5">Sign Up</button>
+    </div>
+  </main>
+</template>
