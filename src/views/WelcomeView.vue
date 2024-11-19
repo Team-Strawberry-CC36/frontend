e004fdc26ca3c7b4ec9a1680ecb0f3c6e79cd1b6
kiw1meister <linandy719@gmail.com>
2024-11-19 15:23:03 +0900

    feat: prototype about page and login page scaffold

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
   <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
@@ -7,7 +9,10 @@
       <h2 class="mt-5">Cultural appreciation begins with understanding</h2>
     </div>
     <div class="mx-auto mt-10">
-      <button class="bg-blueGreen p-3 rounded-xl mx- hover:animate-pulse">Login</button>
+      <!-- <button class="bg-blueGreen p-3 rounded-xl mx- hover:animate-pulse">Login</button> -->
+      <RouterLink class="bg-blueGreen p-3 rounded-xl mx- hover:animate-pulse" to="/login">
+        Login
+      </RouterLink>
       <button class="bg-blueGreen p-3 rounded-xl mx-5 hover:animate-pulse">Sign Up</button>
     </div>
   </main>
