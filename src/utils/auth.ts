f858136d489253da68d1ee3639d5037d8805fdf8
kiw1meister <linandy719@gmail.com>
2024-11-21 16:22:38 +0900

    feat: sign-up components + basic sign-up functions

---
+++ src/utils/auth.ts
@@ -0,0 +1,27 @@
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  //signInWithEmailAndPassword,
+} from 'firebase/auth'
+import { auth } from '@/firebase'
+
+interface RegisterUserResponse {
+  token: string
+  uid: string
+}
+
+export const registerUserThroughFirebase = async (
+  email: string,
+  password: string,
+  displayName: string,
+): Promise<RegisterUserResponse> => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    await updateProfile(userCredential.user, { displayName })
+    const token = await userCredential.user.getIdToken(true)
+    return { token, uid: userCredential.user.uid }
+  } catch (error) {
+    console.error('Error registering user:', error)
+    throw error
+  }
+}
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SignUpComponent from '@/components/SignUp/SignUpComponent.vue'
+</script>
+
+<template>
+  <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
+    <SignUpComponent />
+  </main>
+</template>
