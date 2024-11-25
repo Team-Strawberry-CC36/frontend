de1bea6d4ebf5dfa8e8173e16dedb94c1bb9d98f
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-25 21:18:42 +0900

    build: pass props from ExperienceView to ExperiencesComponent

--- src/views/Experiences/ExperiencesView.vue
+++ src/views/Experiences/ExperiencesView.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import ExperiencesComponent from './ExperiencesComponents/ExperiencesComponent.vue';
+    import ExperiencesComponent from './ExperiencesComponents/ExperiencesComponent.vue';
+    import { ref } from 'vue';
+    import type Place from '@/utils/interfaces/Place';
+    const placeDetails = ref<Place>({
+        id: 0,
+        name: '',
+        address: '',
+        generalInfo: '',
+        placeType: 'onsen',
+        location: {
+            latitude: 0,
+            longitude: 0,
+        },
+        etiquettes: [],
+        experiences: [],
+        photos: [],
+        metadata: {
+            createdAt: new Date,
+            updatedAt: new Date,
+        }
+    });
+
 </script>
 
 <template>
   <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
-    <ExperiencesComponent />
+    <ExperiencesComponent
+        :placeDetails="placeDetails"
+    />
   </main>
 </template>
