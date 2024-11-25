da422d58c7c24f88acf11e96a56e42d9df20eb8c
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-25 21:18:42 +0900

    build: pass props from ExperienceView to ExperiencesComponent

--- src/views/Experiences/ExperiencesComponents/ExperiencesComponent.vue
+++ src/views/Experiences/ExperiencesComponents/ExperiencesComponent.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
+    import type Place from '@/utils/interfaces/Place';
+
+    defineProps<{
+        placeDetails: Place;
+    }>();
 
 </script>
 
 <template>
-
+    <div>
+        <section>
+            <!-- Place Name + add Experiences button-->
+        </section>
+        <section>
+            <!-- Filter Experiences section -->
+        </section>
+        <section>
+            <!-- Repeated divs of experiences -->
+        </section>
+    </div>
 </template>
