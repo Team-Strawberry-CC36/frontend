cb79032bedb07eacc062c8e773f590c2ea5da4a2
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-25 20:59:28 +0900

    build: Experiences component template

---
+++ src/views/Experiences/ExperiencesView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ExperiencesComponent from './ExperiencesComponents/ExperiencesComponent.vue';
+</script>
+
+<template>
+  <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
+    <ExperiencesComponent />
+  </main>
+</template>
