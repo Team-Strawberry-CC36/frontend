1504a736977eaead8ac6026743527a90903579c1
kiw1meister <linandy719@gmail.com>
2024-11-25 20:01:16 +0900

    chore: unify color theme

--- src/views/SignUp/SignUpView.vue
+++ src/views/SignUp/SignUpView.vue
@@ -3,7 +3,7 @@ import SignUpComponent from './SignUpComponents/SignUpComponent.vue';
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
+  <main class="flex flex-col items-center justify-center h-screen w-full bg-mist">
     <SignUpComponent />
   </main>
 </template>
