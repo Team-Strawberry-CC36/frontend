0cd89d87d50b41c02f34ead61b9025ac14fe811f
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-26 14:58:34 +0900

    chore: remove local state from ExperiencesView

--- src/views/Experiences/ExperiencesView.vue
+++ src/views/Experiences/ExperiencesView.vue
@@ -2,25 +2,6 @@
     import ExperiencesComponent from './ExperiencesComponents/ExperiencesComponent.vue';
     //import AddExperiences from './ExperiencesComponents/AddExperience.vue';
     import { ref } from 'vue';
-    import type Place from '@/utils/interfaces/Place';
-    const placeDetails = ref<Place>({
-        id: 0,
-        name: '',
-        address: '',
-        generalInfo: '',
-        placeType: 'onsen',
-        location: {
-            latitude: 0,
-            longitude: 0,
-        },
-        etiquettes: [],
-        experiences: [],
-        photos: [],
-        metadata: {
-            createdAt: new Date,
-            updatedAt: new Date,
-        }
-    });
 
     // State for showing the AddExperienceComponent
     const showAddExperience = ref(false);
