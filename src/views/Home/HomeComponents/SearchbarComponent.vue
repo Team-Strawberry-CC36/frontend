18f95675c1816fece9dc0547898b994e13f5dccb
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-26 18:17:32 +0900

    style: search bar and search button

--- src/views/Home/HomeComponents/SearchbarComponent.vue
+++ src/views/Home/HomeComponents/SearchbarComponent.vue
@@ -2,10 +2,15 @@
     import { usePlaceStore } from '@/stores/PlaceStore';
     const place = usePlaceStore();
     place.useMock();
+
+    const handleSearch = () => {
+
+    }
 </script>
 
 <template>
-    <div>
-        <input type="search" />
+    <div class="flex flex-row justify-center m-1">
+        <input type="search" placeholder="Search for a place" class="border border-charcoal w-4/5 h-10 p-3 mr-1 rounded-xl focus:outline-none bg-mist focus:bg-white text-center text-sm lg:text-2xl"/>
+        <button @click="handleSearch" class="border border-charcoal w-1/5 rounded-xl bg-charcoal text-white hover:cursor-pointer hover:bg-white hover:text-charcoal">Search</button>
     </div>
 </template>
