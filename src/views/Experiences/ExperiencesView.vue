427c175a7ff425b7087cda45b64f681d2e9603a2
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-11-26 14:32:36 +0900

    style: experiences component

--- src/views/Experiences/ExperiencesView.vue
+++ src/views/Experiences/ExperiencesView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import ExperiencesComponent from './ExperiencesComponents/ExperiencesComponent.vue';
+    //import AddExperiences from './ExperiencesComponents/AddExperience.vue';
     import { ref } from 'vue';
     import type Place from '@/utils/interfaces/Place';
     const placeDetails = ref<Place>({
@@ -21,12 +22,20 @@
         }
     });
 
+    // State for showing the AddExperienceComponent
+    const showAddExperience = ref(false);
+
+    // Method to toggle the state
+    const toggleAddExperience = () => {
+        showAddExperience.value = !showAddExperience.value;
+    };
+
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center h-screen w-full bg-roseQuartz">
+  <main class="flex flex-col items-center h-fit w-full bg-mist">
     <ExperiencesComponent
-        :placeDetails="placeDetails"
+        @toggleAddExperience="toggleAddExperience"
     />
   </main>
 </template>
