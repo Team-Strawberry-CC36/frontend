cc689a85bc83833415aab12b21a1870c823b5001
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-12-01 22:21:59 +0900

    update: vite.config.ts to include port

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173,
+  }
 })
