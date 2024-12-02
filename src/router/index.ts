4845847239b10ec0b340032df2a607388d3086a3
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-12-02 11:12:06 +0900

    chore: rename TouristDashboard.vue to TouristDashboardView.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ import SignUpView from '@/views/SignUp/SignUpView.vue';
 import HomeView from '@/views/Home/HomeView.vue';
 import ExperiencesView from '@/views/Experiences/ExperiencesView.vue';
 import TestingComponents from '@/views/testing-components/TestingComponents.vue';
-import TouristDashboard from '@/views/TouristDashboard/TouristDashboard.vue';
+import TouristDashboard from '@/views/TouristDashboard/TouristDashboardView.vue';
 
 const auth = getAuth();
 
