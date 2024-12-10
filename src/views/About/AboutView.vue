edee9f51f7e64d99c15361b06dbfb05d7f9995ff
kiw1meister <linandy719@gmail.com>
2024-12-10 17:45:19 +0900

    feat: carousel on welcome

--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -2,10 +2,10 @@
   <main
     class="flex flex-col items-center justify-center w-full bg-cover bg-center bg-fixed bg-[url('https://prd-static.gltjp.com/glt/data/article/21000/20522/20231116_142745_b1cac14b_w1920.webp')] text-white p-6"
   >
-    <div class="bg-black bg-opacity-50 rounded-lg shadow-lg p-8 max-w-4xl text-center">
+    <div class="bg-charcoal bg-opacity-50 rounded-lg shadow-lg p-8 max-w-4xl text-center">
       <h1 class="text-4xl font-extrabold mb-6">Discover Jappuri</h1>
       <p class="mb-4 text-lg">
-        Japan is a captivating blend of breathtaking landscapes. There are many places and activities to make your journey to 
+        Japan is a captivating blend of breathtaking landscapes. There are many places and activities to make your journey to
         Japan wonderful. But navigating it's culture, traditions, and unspoken etiquettes can be
         hard.
       </p>
--- src/views/Welcome/WelcomeView.vue
+++ src/views/Welcome/WelcomeView.vue
@@ -1,8 +1,36 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+const photolinks = [
+  "https://gaijinpot.scdn3.secure.raxcdn.com/app/uploads/sites/6/2017/09/Ginzan-Onsen-Yamagata2.jpg",
+  "https://travel.rakuten.com/contents/sites/contents/files/styles/max_1300x1300/public/2024-10/meiji-jingu-shrine_2.jpg?itok=HkxmWd-C",
+  "https://d36tnp772eyphs.cloudfront.net/blogs/1/2019/02/Red-Torii-gates-in-Fushimi-Inari-shrine-in-Kyoto-Japan.jpg",
+  "https://jw-webmagazine.com/wp-content/uploads/2019/06/jw-5d147f249c2d87.14657341.jpeg",
+  "https://images.microcms-assets.io/assets/619bb8ae3f944560ae00d12e05c6b72c/14249ba27ac441279e22605d4d1ec2ad/the%20SUSHI_concept_text.webp"
+]
 </script>
 
 <template>
+
+    <div>
+    <div v-if="photolinks.length" class="absolute overflow-hidden w-full">
+      <Carousel :wrapAround="true" :itemsToShow="1" :autoplay="10000">
+        <Slide v-for="url in photolinks" :key="url">
+          <img :src="url" alt="Place Photo" class="w-full object-cover" />
+        </Slide>
+      </Carousel>
+    </div>
+    <div v-else>
+      <p>No photos available</p>
+    </div>
+  </div>
+
+
+
+
+
   <main class="flex flex-col items-center justify-center w-full bg-mist">
     <div class="w-1/2 text-center">
       <h1 class="text-5xl">Welcome to Japuri</h1>
